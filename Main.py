2c781687615a0ff5fa80b9a120ff7a8ef85d1289
Bill Nikolopoulos <71321624+BillNikolo@users.noreply.github.com>
2020-10-19 21:07:21 +0200

    Create the player

--- Main.py
+++ Main.py
@@ -5,26 +5,45 @@ import pygame_assets as assets
 Gray = (128, 128, 128)
 White = (255, 255, 255)
 
-pygame.init() # Initialize the game
+
+class Car(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Car, self).__init__()
+        self.image = pygame.image.load("Car.png").convert()
+        self.rect = self.image.get_rect()
+        self.rect.center = (20, 100)
+
+
+# Initialize the game
+pygame.init()
 
 Width, Height = 600, 400
-screen = pygame.display.set_mode((Width, Height))  # The dimensions of the window of the game
-pygame.display.set_caption("Racing Numbers")  # Create a caption for the game
+# Dimensions of the screen
+screen = pygame.display.set_mode((Width, Height))
+# Title of the screen
+pygame.display.set_caption("Racing Numbers")
 
 # Background
 background = pygame.image.load('Road.png')
 
 
-
-Gameplay = True  # Constant always true until we need to close/quit/exit the game
-
+# Constant always true until we need to close/quit/exit the game
+Gameplay = True
+clock = pygame.time.Clock()
+FPS = 60
+Car = Car()
+car_list = pygame.sprite.Group()
+car_list.add(Car)
 while Gameplay:  # Initializes the main loop of the game
+    clock.tick(FPS)
     # Fills the background with gray
     screen.fill(Gray)
     # Background Image
     screen.blit(background, (0, 0))
     # Update the color and background of the screen
+    car_list.draw(screen)
     pygame.display.update()
+    pygame.display.flip()
     for event in pygame.event.get():  # Events are the inputs of the player
         if event.type == pygame.QUIT:
             Gameplay = False
