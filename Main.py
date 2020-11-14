3b19c5fd3a1bafd00eaa1ca0de81ccc055d4f4b7
Bill Nikolopoulos <71321624+BillNikolo@users.noreply.github.com>
2020-11-14 14:47:39 +0100

    Fixed the Float - Int problem causing Targets and Background having different speed

--- Main.py
+++ Main.py
@@ -8,8 +8,12 @@ import pygame_assets as assets
 Gray = (128, 128, 128)
 White = (255, 255, 255)
 
+#line_list = [(700,85),(700,180),(700,230)]
+LINE1 = (700,85)
+LINE2 = (700,180)
+LINE3 = (700,235)
 BACKGROUND_SPEED = 150
-CAR_VERT_SPEED = 150
+CAR_VERT_SPEED = 180
 
 
 class Background:
@@ -28,8 +32,8 @@ class Background:
 
     def updateBackground(self, dt):
         # Update the coordination of the st/end point
-        self.backgroundX1 -= BACKGROUND_SPEED*dt
-        self.backgroundX2 -= BACKGROUND_SPEED*dt
+        self.backgroundX1 -= int(BACKGROUND_SPEED*dt)
+        self.backgroundX2 -= int(BACKGROUND_SPEED*dt)
         if self.backgroundX1 <= -self.BackgroundRect.width:
             self.backgroundX1 = self.BackgroundRect.width
         if self.backgroundX2 <= -self.BackgroundRect.width:
@@ -56,10 +60,10 @@ class Car(pygame.sprite.Sprite):
         pressed_keys = pygame.key.get_pressed()
         if self.rect.top > 30:
             if pressed_keys[K_UP]:
-                self.rect.move_ip(0, -CAR_VERT_SPEED*dt)
+                self.rect.move_ip(0, int(-CAR_VERT_SPEED*dt))
         if self.rect.bottom < 370:
             if pressed_keys[K_DOWN]:
-                self.rect.move_ip(0, CAR_VERT_SPEED*dt)
+                self.rect.move_ip(0, int(CAR_VERT_SPEED*dt))
 
 
 class StopSign(pygame.sprite.Sprite):
@@ -74,7 +78,10 @@ class StopSign(pygame.sprite.Sprite):
         self.rect.center = (x, y)
         
     def update(self, dt):
-        self.rect.move_ip(-BACKGROUND_SPEED*dt, 0)
+        self.rect.move_ip(int(-BACKGROUND_SPEED*dt), 0)
+
+        if self.rect.x < -50:
+            self.rect.move_ip(random.choice([LINE1, LINE2, LINE3]))
 
 
 class NumTarget(pygame.sprite.Sprite):
@@ -89,10 +96,10 @@ class NumTarget(pygame.sprite.Sprite):
         self.rect.center = (x, y)
         
     def update(self, dt):
-        self.rect.move_ip(-BACKGROUND_SPEED*dt, 0)
+        self.rect.move_ip(int(-BACKGROUND_SPEED*dt), 0)
 
-        if self.rect.x < 0:
-            self.rect.move_ip(600 + random.random()*300, random.random() * 200)
+        if self.rect.x < -50:
+            self.rect.move_ip(int(600 + random.random()*300), int(random.random() * 200))
             
 
 # Initialize the game
@@ -120,42 +127,43 @@ random.shuffle(target_filenames)
 target_group = pygame.sprite.Group()
 x = 700
 for filename in target_filenames:
-    y = int(random.random() * 3) * (Height / 3)
+    y = int(random.random() * 3 * (Height / 3))
     target = NumTarget(filename, x, y)
-    x += 300 + random.random() * 600
+    x += int(300 + random.random() * 600)
     target_group.add(target)
 
 car = Car()
 stop_sign = StopSign(500, 85)
 background = Background()
 
-sprites = pygame.sprite.Group()
-sprites.add(car)#, stop_sign)
-sprites.add(stop_sign)
-sprites.add(target_group)
+car_sprite = pygame.sprite.Group()
+car = Car()
 
-#Adding a new User event
-NEW_EVENT = pygame.USEREVENT + 1
-pygame.time.set_timer(NEW_EVENT, 1000)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(car)
+all_sprites.add(stop_sign)
+all_sprites.add(target_group)
 
 # Initializes the main loop of the game
 while Gameplay:
     # The time running
-    dt = clock.tick(FPS)/1000
+    dt = clock.tick(FPS)/700
 
     for event in pygame.event.get():
         # Events are the inputs of the player
-
-        if event.type == NEW_EVENT:
-            pass#SPEED += 0.5
         if event.type == pygame.QUIT:
             Gameplay = False
+
+        if event.type == pygame.KEYUP or event.type == pygame.KEYDOWN:
+            car_sprite.update()
 
     # Update the background of the screen
     background.updateBackground(dt)
     background.renderBackground()
     # Draw sprites
-    sprites.update(dt)
-    sprites.draw(screen)
+    all_sprites.update(dt)
+    all_sprites.draw(screen)
     # Update the whole screen
     pygame.display.update()
+
+pygame.quit()
